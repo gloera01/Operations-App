0437e68699ab88a9ebdba8da10b80fd3e88872d8
Gustavo Loera <gustavo.loera@arkusnexus.com>
2023-02-20 10:02:24 -0800

    fix default export

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export const OPERATIONS_API_URL = process.env.REACT_APP_OPERATIONS_API_URL;
 
-export default { OPERATIONS_API_URL };
+const config = { OPERATIONS_API_URL };
+
+export default config;
